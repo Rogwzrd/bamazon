a43206b578cf37525c4f6af8cbd1ece69f3efa9f
Michael Daye <mgdaye@gmail.com>
2017-11-01 16:18:45 -0400

    creating inquirer customer input

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -12,9 +12,29 @@ var connection = mysql.createConnection({
     password: "",
     database: "bamazon_db"
 });
+function userPrompt(){
+    inquirer.prompt([
+        {
+            tpe: "confirm",
+            message: "\nHello and welcome to Mike's Bamazon Store\nWould you like to see our inventory?",
+            name: "greeting",
+            default: Y
+        },
 
+]).then(function(answers){
+        //what you gon do with at that data
+        if (answers.greeting === true) {
+            readProducts();
+            connection.end();
+        }
+    })
+}
 function purchaseProduct(id, quantity) {
-    connection.query("UPDATE(stock_ ")
+    connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ? ", [quantity, id], function (err, res) {
+        if (err) throw err;
+        console.log(res);
+    })
+
 }
 
 
@@ -22,13 +42,17 @@ function readProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
-        console.log(`*******************Welcome to the Mike Daye Bamazon Store********************`)
+        console.log(`*******************Products********************`)
         for (var i = 0; i < res.length; i++) {
             console.log(`\nItem ID: ${res[i].item_id}|| Product: ${res[i].product_name}|| Department: ${res[i].department_name}|| Price: ${res[i].price}|| Quantitiy: ${res[i].stock_quantity}`)
         }
         // console.log(res);
-        connection.end();
     });
 }
 
-readProducts();
+userPrompt();
+
+// readProducts();
+// purchaseProduct(3, 2);
+// readProducts();
+// connection.end();
