58c77e1ea05184f5a32a9e96247df263c520a5c6
Michael Daye <mgdaye@gmail.com>
2017-11-02 02:51:35 -0400

    completed base funcitonality of app

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -12,47 +12,93 @@ var connection = mysql.createConnection({
     password: "",
     database: "bamazon_db"
 });
-function userPrompt(){
+var availStock = [];
+var purchaseNum = 0;
+
+function greetingPrompt(){
     inquirer.prompt([
         {
             tpe: "confirm",
             message: "\nHello and welcome to Mike's Bamazon Store\nWould you like to see our inventory?",
             name: "greeting",
-            default: Y
-        },
-
+            default: true
+        }
 ]).then(function(answers){
         //what you gon do with at that data
         if (answers.greeting === true) {
             readProducts();
-            connection.end();
+            purchasePrompt();
         }
+    })
+}
+
+function purchasePrompt() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "\nWhich product would you like?",
+            name: "id"
+        },
+        {
+            type: "inut",
+            message: "\nHow many would you like?",
+            name: "quantity"
+        }
+    ]).then(function(answers){
+        purchaseProduct(answers.id, answers.quantity);
     })
 }
 function purchaseProduct(id, quantity) {
-    connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ? ", [quantity, id], function (err, res) {
+
+    connection.query("SELECT * FROM products WHERE item_id = ?", [id], function(err, res) {
         if (err) throw err;
-        console.log(res);
-    })
-
-}
-
-
-function readProducts() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        // Log all results of the SELECT statement
-        console.log(`*******************Products********************`)
-        for (var i = 0; i < res.length; i++) {
-            console.log(`\nItem ID: ${res[i].item_id}|| Product: ${res[i].product_name}|| Department: ${res[i].department_name}|| Price: ${res[i].price}|| Quantitiy: ${res[i].stock_quantity}`)
+        if (res) {
+            console.log(`The result of the product search for purchase ${JSON.stringify(res, null, 2)}`)
+            availStock.push(res[0]["stock_quantity"]);
+            console.log(`The availabe stock is ${availStock}`);
         }
-        // console.log(res);
-    });
+        checkstock(id, quantity)
+    })
+}
+function checkstock(id, quantity) {
+    if (availStock < quantity) {
+        console.log("Insufficient quantity!")
+        connection.end();
+    } else {
+        connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ? ", [availStock - quantity, id], function (err, res) {
+            if (err) throw err;
+            console.log(`Order Processed!`)
+            readProducts(id, quantity)
+        })
+    }
 }
 
-userPrompt();
+//this function reads the contents of the products table in the bamazon database
+function readProducts(id, quantity) {
 
-// readProducts();
-// purchaseProduct(3, 2);
-// readProducts();
-// connection.end();
+    //if there is no argument then show all of the products
+    if (!id) {
+        connection.query("SELECT * FROM products", function (err, res) {
+            if (err) throw err;
+            // Log all results of the SELECT statement
+            console.log(`*******************Products********************`)
+            for (var i = 0; i < res.length; i++) {
+                console.log(`\nItem ID: ${res[i].item_id}|| Product: ${res[i].product_name}|| Department: ${res[i].department_name}|| Price: ${res[i].price}|| Quantitiy: ${res[i].stock_quantity}`)
+            }
+        });
+        //if there is an argument then read is apart of a callback that follows the purchase prompt
+    } else {
+        connection.query("SELECT * FROM products WHERE item_id = ?", [id],function (err, res) {
+            if (err) throw err;
+            // Log all results of the SELECT statement
+            console.log(`*******************Checkout********************`);
+            for (var i = 0; i < res.length; i++) {
+                // console.log(`\nItem ID: ${res.item_id}|| Product: ${res.product_name}|| Department: ${res.department_name}|| Price: ${res.price}|| Quantitiy: ${res.stock_quantity}`);
+                console.log(`\nEnjoy your ${res[i].product_name}\nYour purchase total is ${res[i].price * quantity}`);
+                connection.end()
+            }
+        });
+    }
+}
+
+greetingPrompt();
